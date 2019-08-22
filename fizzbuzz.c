94d6de8fbb4afe0cae7dee5763ee6c81a0c71812
Gauri Durugkar <52330977+gauri-sd@users.noreply.github.com>
2019-08-22 18:41:54 +0530

    Add files via upload

---
+++ fizzbuzz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void task()
+{
+	int i=0;
+	for(i=1;i<=100;i++)
+	{
+		if(i%5==0)
+		{
+			printf("Buzz\t");
+		}
+		else if(i%3==0)
+		{
+			printf("Fizz\t");
+		}
+		else
+		{
+			printf("%d\t",i);
+		}
+	}
+}
+
+int main()
+{
+	task();
+	return 0;
+}
